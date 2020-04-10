3aad802893d4890005892ce357f0fd4bcc47b022
GoldenDrag <mr.ablaysan@gmail.com>
2020-04-10 08:27:51 +0600

    litle deasign

--- front/src/app/book.ts
+++ front/src/app/book.ts
@@ -1,6 +1,9 @@
+import { Comment } from './comment'
+
 export interface Book {
     id: number;
     author: string;
     title: string;
     description: string;
+    comments: Comment[];
 }
--- front/src/app/mock-books.ts
+++ front/src/app/mock-books.ts
@@ -6,60 +6,234 @@ export const BOOKS: Book[] = [
         author: 'GumboJumbo', 
         title: 'Dr. Nie',
         description: 'Story of doctor Nie, who finally made Obama prism',
-        // comments: [
-        //     rating: 2,
-        //     author: "BigDick",
-        //     comment: "Love doctors",
-        //     date: "20-12-12"
-        // ]
+        comments: [
+            {
+                rating: 5,
+                author: "BigD",
+                comment: "Love doctors",
+                date: "20-12-122012-10-16T17:57:28.556094Z"
+            },
+            {
+                rating: 4,
+                author: 'BigB',
+                comment: 'Love love',
+                date: '2014-10-16T17:57:28.556094Z'
+            },
+            {
+                rating: 3,
+                author: 'DforDiane',
+                comment: 'expected more than just a fart jokes',
+                date: "2016-10-16T17:57:28.556094Z"
+            }
+        ]
     },
     { 
         id: 2, 
         author: 'Jiklopo', 
         title: 'Narcisso',
-        description: 'Narcisso was a pretties boy in a villedge and lived well until uncle returned from war' 
+        description: 'Narcisso was a pretties boy in a villedge and lived well until uncle returned from war',
+        comments: [
+            {
+                rating: 5,
+                author: "BigD",
+                comment: "Narcisso is so cool",
+                date: "20-12-122012-10-16T17:57:28.556094Z"
+            },
+            {
+                rating: 4,
+                author: 'BigB',
+                comment: 'Love Narcisso but the villain is boring',
+                date: '2014-10-16T17:57:28.556094Z'
+            },
+            {
+                rating: 3,
+                author: 'DforDiane',
+                comment: 'boring af',
+                date: "2016-10-16T17:57:28.556094Z"
+            }
+        ] 
     },
     { 
         id: 3, 
         author: 'Ramen', 
         title: 'Boom truck',
-        description: "Kira always wished for quit live with a woman he love, but ghosts from past keep following him" 
+        description: "Kira always wished for quit live with a woman he love, but ghosts from past keep following him",
+        comments: [
+            {
+                rating: 5,
+                author: "BigD",
+                comment: "Love Kira",
+                date: "20-12-122012-10-16T17:57:28.556094Z"
+            },
+            {
+                rating: 4,
+                author: 'BigB',
+                comment: 'Wow those guys are real assholes, we are with you Kira',
+                date: '2014-10-16T17:57:28.556094Z'
+            },
+            {
+                rating: 3,
+                author: 'DforDiane',
+                comment: 'Why they are always shouting?',
+                date: "2016-10-16T17:57:28.556094Z"
+            }
+        ] 
     },
     { 
         id: 4, 
         author: 'Arakin', 
         title: 'DoDo pizza',
-        description: 'Biography of bilioner bird who created world wide famous brand dodo pizza' 
+        description: 'Biography of bilioner bird who created world wide famous brand dodo pizza',
+        comments: [
+            {
+                rating: 5,
+                author: "BigD",
+                comment: "Wow",
+                date: "20-12-122012-10-16T17:57:28.556094Z"
+            },
+            {
+                rating: 4,
+                author: 'BigB',
+                comment: 'Good writing and narrative',
+                date: '2014-10-16T17:57:28.556094Z'
+            },
+            {
+                rating: 3,
+                author: 'DforDiane',
+                comment: 'ending sucks what is the point of KILLING everyone :(',
+                date: "2016-10-16T17:57:28.556094Z"
+            }
+        ] 
     },
     { 
         id: 5, 
         author: 'Ultimate Kars', 
         title: 'Magenta Magenta',
-        description: 'What can you do to achieve your goal? What are ready to pay back? Our MC Magenta gonna find this answers in the road to become a rock' 
+        description: 'What can you do to achieve your goal? What are ready to pay back? Our MC Magenta gonna find this answers in the road to become a rock',
+        comments: [
+            {
+                rating: 2,
+                author: "BigD",
+                comment: "Fuck Magenta",
+                date: "20-12-122012-10-16T17:57:28.556094Z"
+            },
+            {
+                rating: 3,
+                author: 'BigB',
+                comment: 'fuck him',
+                date: '2014-10-16T17:57:28.556094Z'
+            },
+            {
+                rating: 1,
+                author: 'DforDiane',
+                comment: 'MC is too gross',
+                date: "2016-10-16T17:57:28.556094Z"
+            }
+        ] 
     },
     { 
         id: 6, 
         author: 'DioDiego', 
         title: 'Scary WARUDO',
-        description: 'romantic story about love between dinosaur and diver' 
+        description: 'romantic story about love between dinosaur and diver',
+        comments: [
+            {
+                rating: 2,
+                author: "Danny",
+                comment: "I will never forgive you",
+                date: "20-12-122012-10-16T17:57:28.556094Z"
+            },
+            {
+                rating: 1,
+                author: 'BigB',
+                comment: 'My eyes, my butifull eyes!',
+                date: '2014-10-16T17:57:28.556094Z'
+            },
+            {
+                rating: 3,
+                author: 'DforDiane',
+                comment: 'kinda funny in its badness',
+                date: "2016-10-16T17:57:28.556094Z"
+            }
+        ] 
     },
     { 
         id: 7, 
         author: 'JohnJoe', 
         title: 'Stone Mask',
-        description: 'Archeologist George after 30 years of search finds ancient Aztec underground tomb. What secrets it hides'
+        description: 'Archeologist George after 30 years of search finds ancient Aztec underground tomb. What secrets it hides',
+        comments: [
+            {
+                rating: 4,
+                author: "BigD",
+                comment: "Predictible but intertaining",
+                date: "20-12-122012-10-16T17:57:28.556094Z"
+            },
+            {
+                rating: 4,
+                author: 'BigB',
+                comment: 'focus more on mystery part',
+                date: '2014-10-16T17:57:28.556094Z'
+            },
+            {
+                rating: 5,
+                author: 'DforDiane',
+                comment: 'I love MC',
+                date: "2016-10-16T17:57:28.556094Z"
+            }
+        ]
     },
     { 
         id: 8, 
         author: 'Narcosha21', 
         title: 'Nautra',
-        description: 'Little boy who had no place in his hometown makes a deal with the devil to change his boring life'
+        description: 'Little boy who had no place in his hometown makes a deal with the devil to change his boring life',
+        comments: [
+            {
+                rating: 3,
+                author: "BigD",
+                comment: "meh",
+                date: "20-12-122012-10-16T17:57:28.556094Z"
+            },
+            {
+                rating: 3,
+                author: 'BigB',
+                comment: 'meh',
+                date: '2014-10-16T17:57:28.556094Z'
+            },
+            {
+                rating: 3,
+                author: 'DforDiane',
+                comment: 'meh',
+                date: "2016-10-16T17:57:28.556094Z"
+            }
+        ]
     },
     { 
         id: 9, 
         author: 'Gaymaster', 
         title: 'Neat Heaven',
-        description: 'Old scum seeks heaven. Can he achive it?'
+        description: 'Old scum seeks heaven. Can he achive it?',
+        comments: [
+            {
+                rating: 3,
+                author: "BigD",
+                comment: "meh",
+                date: "20-12-122012-10-16T17:57:28.556094Z"
+            },
+            {
+                rating: 3,
+                author: 'BigB',
+                comment: 'meh',
+                date: '2014-10-16T17:57:28.556094Z'
+            },
+            {
+                rating: 3,
+                author: 'DforDiane',
+                comment: 'meh',
+                date: "2016-10-16T17:57:28.556094Z"
+            }
+        ]
     }
     // { id: 10, author: 'Rohan', title: 'Made in Heaven' },
     // { id: 11, author: 'GoldenDrag', title: 'Stone free' },
