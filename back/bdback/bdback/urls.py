050d13cac9d5c17bd7881e051522761bf35235e0
GoldenDrag <mr.ablaysan@gmail.com>
2020-04-24 08:13:46 +0600

    mmm

--- back/bdback/bdback/urls.py
+++ back/bdback/bdback/urls.py
@@ -21,5 +21,4 @@ urlpatterns = [
     path('books_cbv/', include('books_cbv.urls', namespace='books_cbv')),
     path('books_fbv/', include('books_fbv.urls', namespace='books_fbv')),
     path('books_fbv_user/', include('books_fbv_user.urls', namespace='books_fbv_user')),
-
 ]
